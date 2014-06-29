8f5b76b54ca273540daee8ec8c0f44f72932613c
Ari Firmanto <arifirmanto@gmail.com>
2014-06-29 18:24:15 +0700

    revised capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,8 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:dedennufan/#{application}.git"
-set :stages, ["development", "production"]
-set :default_stage, "development"
+set :stages, %w{staging production}
+set :default_stage, "staging"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
